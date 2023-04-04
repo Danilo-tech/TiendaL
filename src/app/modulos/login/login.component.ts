c3e1d9fdb30bab02550e48f2c5f29ca6da02fd84
danilo <ddggmy2@gmail.com>
2023-04-03 19:54:16 -0500

    aplicando login

--- src/app/modulos/login/login.component.ts
+++ src/app/modulos/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+email:any;
+clave:any;
+usuario:any;
+error=false;
+user = {
+  nombre: "",
+  usuario:"",
+  clave: "",
+  tipo:"",
+}
+
+  constructor(private slogin: LoginService, private router:Router){
+
+  }
+
+  ngOnInit():void {
+
+  }
+
+  consulta() {
+    this.slogin.consultar(this.email,this.clave).subscribe((result:any) =>
+      {
+        this.usuario = result;
+        console.log(this.usuario);
+      }
+    )
+    }
 
 }
